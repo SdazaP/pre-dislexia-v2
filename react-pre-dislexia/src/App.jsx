79b18eeebcba64e86014c7b84454f03017540d25
SdazaP <creepersdp57@gmail.com>
2025-09-30 10:41:09 -0600

    refactor: wrap assessment routes with DiagnosticoProvider

--- react-pre-dislexia/src/App.jsx
+++ react-pre-dislexia/src/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./Pages/Home";
 import Evaluaciones from "./Pages/Evaluaciones";
@@ -21,28 +21,59 @@ import PatronFiguras from "./Pages/Pruebas/PatronFiguras";
 import CuestionarioPadres from "./Pages/Pruebas/CuestionarioPadres";
 import ReporteDiagnostico from "./Pages/Pruebas/Reporte/ReporteDiagnostico";
 
+import DiagnosticoProvider from "./context/context";
+import PreReporte from "./Pages/Pruebas/PreReporte";
+
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route exact path="/" element={<Home/>}></Route>
-        <Route exact path="/evaluaciones" element={<Evaluaciones/>}></Route>
-        <Route exact path="/acerca-de" element={<Acerca/>}></Route>
-        <Route exact path="/articulo" element={<ArticuloDislexia/>}></Route>
-        <Route exact path="/contacto" element={<Contacto/>}></Route>
-        <Route exact path="/catalogo-instituciones" element={<CatalogoInstituciones/>}></Route>
-        <Route exact path="/evaluacion/registro" element={<Registro/>}></Route>
-        <Route path="/evaluacion/pre-inicio" element={<PreInicio/>} />
+    <DiagnosticoProvider>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/evaluaciones" element={<Evaluaciones />}></Route>
+          <Route exact path="/acerca-de" element={<Acerca />}></Route>
+          <Route exact path="/articulo" element={<ArticuloDislexia />}></Route>
+          <Route exact path="/contacto" element={<Contacto />}></Route>
+          <Route
+            exact
+            path="/catalogo-instituciones"
+            element={<CatalogoInstituciones />}
+          ></Route>
+          <Route
+            exact
+            path="/evaluacion/registro"
+            element={<Registro />}
+          ></Route>
+          <Route path="/evaluacion/pre-inicio" element={<PreInicio />} />
 
-        <Route path="/evaluacion/palabra-imagen" element={<PalabraImagen/>} />
-        <Route path="/evaluacion/completa-palabra" element={<CompletaPalabra/>} />
-        <Route path="/evaluacion/patron-figuras" element={<PatronFiguras/>} />
-        <Route path="/evaluacion/cuestionario-padres" element={<CuestionarioPadres/>} />
+          <Route
+            path="/evaluacion/palabra-imagen"
+            element={<PalabraImagen />}
+          />
+          <Route
+            path="/evaluacion/completa-palabra"
+            element={<CompletaPalabra />}
+          />
+          <Route
+            path="/evaluacion/patron-figuras"
+            element={<PatronFiguras />}
+          />
+          <Route
+            path="/evaluacion/cuestionario-padres"
+            element={<CuestionarioPadres />}
+          />
 
-        <Route path="/evaluacion/reporte" element={<ReporteDiagnostico/>} />
-      </Routes>
-    </Router>
+          <Route
+            path="/evaluacion/pre-reporte"
+            element={<PreReporte/>}
+          />
+
+
+          <Route path="/evaluacion/reporte" element={<ReporteDiagnostico />} />
+        </Routes>
+      </Router>
+    </DiagnosticoProvider>
   );
 }
 
