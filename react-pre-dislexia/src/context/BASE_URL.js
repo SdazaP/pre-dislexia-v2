55f9ce719de27277790238430045295cdaf35412
SdazaP <creepersdp57@gmail.com>
2025-10-06 14:06:03 -0600

    fix: correct syntax errors in BASE_URL.js

--- react-pre-dislexia/src/context/BASE_URL.js
+++ react-pre-dislexia/src/context/BASE_URL.js
@@ -1,2 +1,2 @@
-/* export const BASE_URL = "http://localhost:3000"  /* */ Prueba en local */
-export const BASE_URL = "https://pre-dislexia-v2.vercel.app"  /* URL del servidor */
+/* export const BASE_URL = "http://localhost:3000" */    /* Prueba en local */
+export const BASE_URL = "https://pre-dislexia-v2.vercel.app"  /* URL del servidor */ 
